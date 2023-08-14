2fa78bdd076d56846b1c3f34e5f13a2d36de22ec
victor <victorpedr879@gmail.com>
2023-08-14 00:25:58 -0300

    feat: footer finished

--- src/presentation/components/footer/footer-styled.ts
+++ src/presentation/components/footer/footer-styled.ts
@@ -1,9 +1,9 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  background-color: #f4f3f4;
-  color: #000;
+  color: ${({ theme }) => theme.colors.primary};
   padding: 1rem 0;
+  border-top: 2px solid #6550f3;
 `;
 
 export const Width = styled.div`
@@ -12,10 +12,46 @@ export const Width = styled.div`
 `;
 
 export const Box = styled.div`
-  margin: 3.5rem 0rem;
   display: flex;
-  justify-content: center;
   align-items: center;
-  flex-direction: column;
-  text-align: center;
+  justify-content: space-between;
+  margin: 2.5rem 0rem;
+`;
+
+export const Text = styled.div`
+  max-width: 600px;
+  h1 {
+    display: flex;
+    align-items: center;
+    font-size: 1.8rem;
+    span {
+      display: flex;
+      align-items: center;
+      padding-right: 0.5rem;
+      font-size: 2.2rem;
+    }
+  }
+`;
+
+export const Links = styled.div`
+  display: flex;
+`;
+
+export const Quicklinks = styled.div`
+  h1 {
+    font-size: 1.8rem;
+  }
+  ul {
+    list-style: none;
+  }
+`;
+
+export const Contact = styled.div`
+  padding-left: 5rem;
+  h1 {
+    font-size: 1.8rem;
+  }
+  ul {
+    list-style: none;
+  }
 `;
--- src/presentation/components/footer/footer.tsx
+++ src/presentation/components/footer/footer.tsx
@@ -1,10 +1,58 @@
-import { Container, Width, Box } from './footer-styled';
+import { ICON } from 'presentation/assets';
+
+import {
+  Container,
+  Width,
+  Box,
+  Text,
+  Links,
+  Quicklinks,
+  Contact,
+} from './footer-styled';
 
 export const Footer = () => {
   return (
     <Container>
       <Width>
-        <Box>Footer</Box>
+        <Box>
+          <Text>
+            <h1>
+              <span>
+                <ICON.IoCarSportSharp />
+              </span>
+              Auto Luxury
+            </h1>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Voluptatem dolor quae adipisci quo a, cumque laudantium ex
+              obcaecati nostrum natus quisquam accusantium, corporis
+              exercitationem tempora nobis temporibus neque. Dignissimos,
+              magnam?
+            </p>
+          </Text>
+          <Links>
+            <Quicklinks>
+              <h1>Links rápidos</h1>
+              <ul>
+                <li>asdadsad</li>
+                <li>asdadsad</li>
+                <li>asdadsad</li>
+                <li>asdadsad</li>
+                <li>asdadsad</li>
+              </ul>
+            </Quicklinks>
+            <Contact>
+              <h1>Contato</h1>
+              <ul>
+                <li>asdadsad</li>
+                <li>asdadsad</li>
+                <li>asdadsad</li>
+                <li>asdadsad</li>
+                <li>asdadsad</li>
+              </ul>
+            </Contact>
+          </Links>
+        </Box>
       </Width>
     </Container>
   );
