df0f58f8ce21be53de532e4ece95273901d00b4b
victor <victorpedr879@gmail.com>
2023-08-14 16:30:22 -0300

    merge

--- src/presentation/pages/register/components/Content.tsx
+++ src/presentation/pages/register/components/Content.tsx
@@ -1,10 +1,44 @@
-import { Container, Width, Box } from './ContentStyles';
+import {
+  Container,
+  Width,
+  Box,
+  Form,
+  Username,
+  Email,
+  Password,
+  Submit,
+} from './ContentStyles';
 
 export const Content = () => {
   return (
     <Container>
       <Width>
-        <Box>Olá</Box>
+        <Box>
+          <Form>
+            <h1> Cadastrar</h1>
+            <p>Crie uma nova conta com seu nome de usuário, e-mail e senha</p>
+            <Username>
+              <label htmlFor="text1">Nome de usuário:</label>
+              <input type="text" name="text" id="text1" />
+            </Username>
+            <Email>
+              <label htmlFor="email1">E-mail:</label>
+              <input type="email" name="email" id="email1" />
+            </Email>
+            <Password>
+              <label htmlFor="password1">Senha</label>
+              <input type="password" name="password" id="password1" />
+            </Password>
+            <Submit>
+              <input
+                type="submit"
+                name="submit"
+                id="submit1"
+                value="Cadastrar"
+              />
+            </Submit>
+          </Form>
+        </Box>
       </Width>
     </Container>
   );
