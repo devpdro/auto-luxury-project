6ef682d995bdd31880572e12edda52f83e2cf2d9
victor <victorpedr879@gmail.com>
2023-08-13 23:29:09 -0300

    feat: finished faqs

--- src/presentation/components/main/questions/questions-styled.ts
+++ src/presentation/components/main/questions/questions-styled.ts
@@ -1,4 +1,15 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+
+const fadeIn = keyframes`
+ from {
+    opacity: 0;
+    transform: translateY(-10px);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+`;
 
 export const Container = styled.div`
   background-color: ${({ theme }) => theme.colors.quinary};
@@ -26,11 +37,24 @@ export const Box = styled.div`
   box-shadow:
     rgba(0, 0, 0, 0.16) 0px 10px 36px 0px,
     rgba(0, 0, 0, 0.06) 0px 0px 0px 1px;
-  margin: 0rem 10rem;
-  transition: all 0.4 ease;
+  margin: 5rem 10rem;
+  border-radius: 25px;
   h1 {
-    padding: 1rem;
+    padding: 0.8rem 1rem 0.5rem 1rem;
     text-align: left;
-    font-size: 1.4rem;
+    font-size: 1.3rem;
   }
+`;
+
+export const Line = styled.hr`
+  margin: 0.5rem 1rem;
+  background-color: red;
+`;
+
+export const Answer = styled.p`
+  font-size: 1.1rem;
+  opacity: 0;
+  transform: translateY(-10px);
+  animation: ${fadeIn} 0.5s forwards;
+  padding: 0rem 1rem 0rem 1rem;
 `;
--- src/presentation/components/main/questions/questions.tsx
+++ src/presentation/components/main/questions/questions.tsx
@@ -1,13 +1,51 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 
-import { Container, Width, Box, Title } from './questions-styled';
+import { Container, Width, Box, Title, Answer, Line } from './questions-styled';
 
-export const Questions = () => {
-  const [showText, setShowText] = useState(false);
+interface QUESTIONSItem {
+  question: string;
+  answer: string;
+}
 
-  const toggleText = () => {
-    setShowText(!showText);
+export const Questions: React.FC = () => {
+  const [showText, setShowText] = useState<{ [index: number]: boolean }>({});
+
+  const toggleText = (index: number) => {
+    setShowText((prevState) => ({
+      ...prevState,
+      [index]: !prevState[index],
+    }));
   };
+
+  const questions: QUESTIONSItem[] = [
+    {
+      question:
+        'Qual é a faixa de preços para alugar um carro de luxo por um dia?',
+      answer:
+        'Os preços podem variar de $500 a $2000 ou mais, dependendo do modelo.',
+    },
+    {
+      question: 'É possível alugar um veículo de luxo com motorista?',
+      answer: 'Sim, oferecemos opções de aluguel com motorista particular.',
+    },
+    {
+      question:
+        'É necessário ter um seguro especial para alugar um veículo de luxo?',
+      answer: 'Sim, exigimos um seguro específico para cobrir danos.',
+    },
+    {
+      question: 'A locadora oferece assistência 24 horas em caso de problemas?',
+      answer:
+        'Sim, a locadora fornece assistência 24h por dia em caso de problemas.',
+    },
+    {
+      question:
+        'Qual é a política de combustível? O veículo deve ser devolvido com tanque cheio?',
+      answer:
+        'Sim, o veículo deve ser devolvido com o tanque de combustível cheio, como na retirada.',
+    },
+  ];
+
   return (
     <Container>
       <Width>
@@ -16,13 +54,13 @@ export const Questions = () => {
           <h1>Perguntas frequentes feitas pelos clientes</h1>
         </Title>
         <Box>
-          <h1 onClick={toggleText}>Quais são as formas de pagamento</h1>
-          {showText && (
-            <p>
-              Aqui está a resposta para a pergunta frequente sobre formas de
-              pagamento.
-            </p>
-          )}
+          {questions.map((questions, index) => (
+            <div key={index}>
+              <h1 onClick={() => toggleText(index)}>{questions.question}</h1>
+              {showText[index] && <Answer>{questions.answer}</Answer>}
+              <Line />
+            </div>
+          ))}
         </Box>
       </Width>
     </Container>
